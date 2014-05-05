fd4c563708c4712c36a87bdf55f5b683f68af4de
zucknick <zucknick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-05 22:09:11 +0000

    improved version with beta(a_i,b_i) prior for pi_i and prior for C
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bvsflex@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/logisticVS.R
+++ pkg/R/logisticVS.R
@@ -1,10 +1,9 @@
 #Logistic Bayesian variable selection (with 'singleGibbs' block Gibbs sampler for updating GAM and beta), with flexible variable selection prior Pi, allow input of (empirical) distribution for Pi in form of a matrix 
-#Manuela Zucknick, 21-05-2011, last modified 13-11-2013
+#Manuela Zucknick & Ana Corberan, 21-05-2011, last modified 05-05-2014
 
-logisticVS <- function(X, Y, b, v, Pi, block=NULL, MCMC, thinn=1, seed=1234, outdir=NULL){
-# Pi = either a scalar, if the prior selection probability pi_i is the same for all variable indices i
-#	or a vector of length p, if the prior selection probabilities pi_i can vary, but has no associated distribution
-#	or a (#subsamples x p) matrix of subsamples, if for each i (=1,...,p) an empirical distribution for pi_i is provided.
+logisticVS <- function(X, Y, b, v, k, d, block=NULL, MCMC, thinn=1, seed=1234, outdir=NULL){
+# k = a scalar specifying the overall expected number of variables in a model (E(Pi[i] = k/p)
+# d = a vector of length p containing the distances for variables i=1,...,p
   
   is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
   
@@ -29,24 +28,17 @@ logisticVS <- function(X, Y, b, v, Pi, block=NULL, MCMC, thinn=1, seed=1234, out
   if(length(v) != p) stop("Length of v has to be the same as ncol(X) = p.\n");
   if(ncol(block) != p | nrow(block) != p) stop("Dimension of 'block' has to be p x p, where p = ncol(X).\n");
   
-  if(length(Pi) == 1) {
-    Pi <- matrix(rep(Pi, p), nrow=1);
-  }else{
-    if(length(Pi) == p) {
-	    Pi <- matrix(Pi, nrow=1);
-    }else{
-      stop("Pi should be either a scalar, a vector of length p or a matrix of dimension Pi_size x p, where p = ncol(X).\n");
-    }
-  }
   if(!is.wholenumber(MCMC) | MCMC<=0) stop("'MCMC' is the number of MCMC iterations and hence has to be a positive integer.\n")	
   if(!is.wholenumber(thinn) | MCMC<=thinn) stop("Every 'thinn'-ed MCMC iteration will be kept (i.e. if 'thinn'=1 then all iterations are kept, if 'thinn'=10 then only every tenth iteration is returned for posterior inference. Consequently, 'thinn' has to be a positive integer.\n")
   
-  Pi_size = nrow(Pi);
+  if(any(d < 0)) stop("'d' is a distance and hence no value can be negative.\n")
+  if(!is.wholenumber(k) | k<=0 | k>p) stop("'k' has to be a positive integer smaller than 'p'.\n")
   
   cat("Starting MCMC...\n");
   .C("logisticVS", 
       X = as.double(X), Y = as.double(Y), n = as.integer(n), p = as.integer(p),
-      b = as.double(b), v = as.double(v), Pi = as.double(Pi), Pi_size = as.integer(Pi_size), 
+      b = as.double(b), v = as.double(v),
+      k = as.integer(k), d = as.double(d),
       block = as.integer(block), MCMC = as.integer(MCMC), 
       thinn = as.integer(thinn),
       PACKAGE = "bvsflex");   
